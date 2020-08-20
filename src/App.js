e2e71f92400a67dbc4f6bd7a96b92d9daea84a10
Ahmed Ch <ahmedchouihi@gmail.com>
2020-08-20 20:40:12 +0100

    Route parameters and HTML slicing part 1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import PageWrapper from './components/PageWrapper';
-import Home from './components/Pages/Home';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+
+//Pages
+import Home from './components/Pages/Home'
+import About from './components/Pages/About';
+
 
 function App() {
     return (
         <Router>
-        <PageWrapper>
+            <PageWrapper>
 
                 <Route
+                    exact={true}
                     path="/"
                     component={Home}/>
+                <Route
+                path="/about"
+                component={About}
+                />
 
-
-        </PageWrapper>
+            </PageWrapper>
         </Router>
     );
 }
