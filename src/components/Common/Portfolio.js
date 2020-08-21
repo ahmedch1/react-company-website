e2bd7af362b423c9f068543135f56553fb040d5f
Ahmed Ch <ahmedchouihi@gmail.com>
2020-08-21 03:58:48 +0100

    Services and Portfolio Pages Part 2

--- src/components/Common/Portfolio.js
+++ src/components/Common/Portfolio.js
@@ -1,35 +1,41 @@
 import React, {Component} from "react";
+import PortfolioItem from "./PortfolioItem";
+import img1 from'../assets/img/portfolio/01-thumbnail.jpg'
+import img2 from'../assets/img/portfolio/02-thumbnail.jpg'
+import img3 from'../assets/img/portfolio/03-thumbnail.jpg'
+import img4 from'../assets/img/portfolio/04-thumbnail.jpg'
+import img5 from'../assets/img/portfolio/05-thumbnail.jpg'
+import img6 from'../assets/img/portfolio/06-thumbnail.jpg'
 
-class Portfolio extends Component{
-render() {
-    return(
-        <section className="bg-light" id="portfolio">
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-12 text-center">
-                        <h2 className="section-heading text-uppercase">Portfolio</h2>
-                        <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-md-4 col-sm-6 portfolio-item">
-                        <a className="portfolio-link" data-toggle="modal" href="#portfolioModal1">
-                            <div className="portfolio-hover">
-                                <div className="portfolio-hover-content">
-                                    <i className="fas fa-plus fa-3x"></i>
-                                </div>
-                            </div>
-                            <img className="img-fluid" src="img/portfolio/01-thumbnail.jpg" alt=""/>
-                        </a>
-                        <div className="portfolio-caption">
-                            <h4>Threads</h4>
-                            <p className="text-muted">Illustration</p>
+const portfolio = [
+    {title: 'Threads', subtitle: 'Illustration', image: img1},
+    {title: 'Explore', subtitle: 'Graphic Design', image: img2},
+    {title: 'Finish', subtitle: 'Identity', image: img3},
+    {title: 'Lines', subtitle: 'Branding', image: img4},
+    {title: 'Southwest', subtitle: 'website design', image: img5},
+    {title: 'window', subtitle: 'photography', image: img6},
+];
+
+class Portfolio extends Component {
+    render() {
+        return (
+            <section className="bg-light" id="portfolio">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-12 text-center">
+                            <h2 className="section-heading text-uppercase">Portfolio</h2>
+                            <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
                         </div>
                     </div>
+                    <div className="row">
+                        {portfolio.map((item, index) => {
+                            return <PortfolioItem{...item} key={index}/>
+                        })}
+                    </div>
                 </div>
-            </div>
-        </section>
-    )
+            </section>
+        )
+    }
 }
-}
+
 export default Portfolio
--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -2,8 +2,10 @@ import React, {Component} from 'react';
 import Header from '../Common/Header';
 import image from '../assets/img/header-bg.jpg';
 
+
 //Re-usable components
 import Services from '../Common/Services';
+import Portfolio from "../Common/Portfolio";
 class Home extends Component {
     render() {
         return (
@@ -17,6 +19,7 @@ class Home extends Component {
                     image={image}
                 />
                 <Services/>
+                <Portfolio/>
             </div>
         )
     }
